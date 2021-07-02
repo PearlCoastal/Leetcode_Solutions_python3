d59f31ac11e8402b138bd867e77b263fced9bd0e
PearlCoastal <treasuresff@gmail.com>
2021-07-02 16:37:07 +0900

    0702_01背包

---
+++ DynamicProcessing/0-1_Backpack.py
@@ -0,0 +1,17 @@
+
+class Solution():
+    def backpack_dp01(self, nums: [int], max_backpack: int, weight: [int], value: [int]):
+
+
+
+nums = [1,5,11,5]
+
+max_backpack = 11
+
+weight = 
+
+value = 
+
+ob = Solution()
+ans = ob.backpack_dp01(nums, max_backpack, weight, value)
+ans
--- DynamicProcessing/416.分割等和子集.py
+++ DynamicProcessing/416.分割等和子集.py
@@ -7,30 +7,30 @@
 # @lc code=start
 class Solution:
     def canPartition(self, nums: [int]) -> bool:
-
-        if sum(nums) % 2 == 1:
+        total = sum(nums)
+        if total % 2 == 1:
             return False
 
-        target = sum(nums) // 2
+        target = total // 2
 
         dp = [[False for i in range(target + 1)] for j in range(len(nums) + 1)]
 
         for i in range(len(nums) + 1):
             dp[i][0] = True
-        
-        for i in range(1, len(nums)):
+
+        for i in range(1, len(nums) + 1):
             for j in range(1, target + 1):
 
                 if j >= nums[i - 1]:
-                    dp[i][j] = dp[i - 1][j] or dp[i - 1][j - nums[i]]
+                    dp[i][j] = dp[i - 1][j] or dp[i - 1][j - nums[i - 1]]
                 else:
                     dp[i][j] = dp[i - 1][j]
         
-        return dp[len(nums)][-1]
+        return dp[len(nums)][target]
         
 # @lc code=end
 
-nums = [1,2,3,6]
+nums = [1,5,11,5]
 
 ob = Solution()
 ans = ob.canPartition(nums)
